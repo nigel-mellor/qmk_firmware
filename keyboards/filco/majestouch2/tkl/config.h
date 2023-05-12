d1144605c3cb752425e17177a6ec095692b14134
Nigel <nigel@Goddard.local>
2023-05-12 21:29:43 +0100

    Pegasus Hoof and Xiudi XD87 customisation

--- keyboards/filco/majestouch2/tkl/config.h
+++ keyboards/filco/majestouch2/tkl/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 Danny Nguyen <danny@hexwire.com>
+Copyright 2016 Daniel Svensson <dsvensson@gmail.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -14,20 +14,13 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-
 #pragma once
-
-#include "config_common.h"
 
 /* key matrix size */
 #define MATRIX_ROWS 8
 #define MATRIX_COLS 18
 
-/* COL2ROW or ROW2COL */
-#define DIODE_DIRECTION COL2ROW
-
-/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-#define DEBOUNCE 5
+#define DYNAMIC_KEYMAP_LAYER_COUNT 3  // not enough memory for a 4th layer with VIA
 
 #undef LOCKING_SUPPORT_ENABLE
 #undef LOCKING_RESYNC_ENABLE
